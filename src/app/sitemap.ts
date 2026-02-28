5e043144b610df9c4b0b8d46b3b1ea0d37ae09f3
richardharmer <mowenxiao@qq.com>
2026-03-01 04:18:43 +0800

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import versionsData from '@/data/versions.json';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const baseUrl = 'https://distanthorizonsguide.com';
 
