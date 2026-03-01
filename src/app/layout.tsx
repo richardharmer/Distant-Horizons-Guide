e1e2a957c59412f4fdd2d0c5961930569c2d3740
richardharmer <mowenxiao@qq.com>
2026-03-01 22:39:55 +0800

    添加clarity统计代码到head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -107,6 +107,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark scroll-smooth">
       <head>
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "vozzwftmwa");
+            `,
+          }}
+        />
+
         {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-89V8QD3P9T"></script>
         <script
